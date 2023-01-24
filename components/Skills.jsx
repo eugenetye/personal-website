fff825b579fa8a76d996721653600606ded87077
Eugene Tye <eugenetye123@gmail.com>
2023-01-24 17:08:27 -0500

    updated skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -14,6 +14,7 @@ import githubImg from '../public/assets/skills/github1.png'
 import nextjs from '../public/assets/skills/nextjs.png'
 import tensorflow from '../public/assets/skills/tensorflow.png'
 import keras from '../public/assets/skills/keras.png'
+import opencv from '../public/assets/skills/opencv.png'
 
 const Skills = () => {
     return (
@@ -177,6 +178,18 @@ const Skills = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                        <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                            <div className='m-auto'>
+                                <Image src={opencv} alt='/' width='64' height='64'/>
+                            </div>
+                            <div className='flex flex-col items-center justify-center'>
+                                <h3>OpenCV</h3>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </div>
             </div>
